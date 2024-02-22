f1fbe563e3d6d72eae62850bff6f333aff9ac8ee
Trương Thành Ä?ại <thanhdaibl1572002@gmail.com>
2024-02-22 22:19:45 +0700

    Update

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from utils import (
-    is_vietnamese,
-    predict_sentiment_svm,
-    # predict_sentiment_phobert,
-)
+from utils import ( is_vietnamese, predict_sentiment_svm )
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/predict/svm", methods=["POST"])
+@app.route("/predict", methods=["POST"])
 def predict_svm():
     try:
         comment = request.json.get("comment", "")
@@ -20,17 +16,3 @@ def predict_svm():
 
     except Exception as e:
         return jsonify({"error": str(e)})
-
-# @app.route("/predict/phobert", methods=["POST"])
-# def predict_phobert():
-#     try:
-#         comment = request.json.get("comment", "")
-#         if is_vietnamese(comment):
-#             return jsonify(predict_sentiment_phobert(comment))
-#         else:
-#             return jsonify(-2)
-#     except Exception as e:
-#         return jsonify({"error": str(e)})
-
-if __name__ == "__main__":
-    app.run(debug=True)
