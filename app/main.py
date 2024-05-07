ea8f5c257c8bfd89076bf41aa844bc3a69847200
Trương Thành Ä?ại <124680705+thanhdaibl1572002@users.noreply.github.com>
2024-05-07 15:19:18 +0700

    Update

--- app/main.py
+++ app/main.py
@@ -1,18 +1,49 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from utils import ( is_vietnamese, predict_sentiment_svm )
+from utils import is_vietnamese, predict_sentiment, predict_credit
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/predict", methods=["POST"])
-def predict_svm():
+@app.route("/sentiment", methods=["POST"])
+def sentiment():
     try:
         comment = request.json.get("comment", "")
         if is_vietnamese(comment):
-            return jsonify(predict_sentiment_svm(comment))
+            return jsonify(predict_sentiment(comment))
         else:
             return jsonify(-2)
 
     except Exception as e:
         return jsonify({"error": str(e)})
+    
+@app.route("/credit", methods=["POST"])
+def credit():
+    try:
+        code_gender = request.json.get('CODE_GENDER')
+        flag_own_car = request.json.get('FLAG_OWN_CAR')
+        flag_own_realty = request.json.get('FLAG_OWN_REALTY')
+        cnt_children = request.json.get('CNT_CHILDREN')
+        amt_income_total = request.json.get('AMT_INCOME_TOTAL')
+        name_income_type = request.json.get('NAME_INCOME_TYPE')
+        name_education_type = request.json.get('NAME_EDUCATION_TYPE')
+        name_family_status = request.json.get('NAME_FAMILY_STATUS')
+        name_housing_type = request.json.get('NAME_HOUSING_TYPE')
+        days_birth = request.json.get('DAYS_BIRTH')
+        days_employed = request.json.get('DAYS_EMPLOYED')
+        flag_work_phone = request.json.get('FLAG_WORK_PHONE')
+        flag_phone = request.json.get('FLAG_PHONE')
+        flag_email = request.json.get('FLAG_EMAIL')
+        occupation_type = request.json.get('OCCUPATION_TYPE')
+        cnt_fam_members = request.json.get('CNT_FAM_MEMBERS')
+        months_balance = request.json.get('MONTHS_BALANCE')
+
+        prediction = predict_credit(code_gender, flag_own_car, flag_own_realty, cnt_children, amt_income_total, name_income_type, name_education_type, name_family_status, name_housing_type, days_birth, days_employed, flag_work_phone, flag_phone, flag_email, occupation_type, cnt_fam_members, months_balance)
+        
+        return jsonify(prediction) 
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+if __name__ == "__main__":
+    app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import re
 import string
 import pickle
+import pandas as pd
 from unidecode import unidecode
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
 
 clear_pattern = re.compile(
     "["
@@ -69,7 +73,7 @@ def is_vietnamese(text):
     return unidecode(text) != text
 
 
-def predict_sentiment_svm(text):
+def predict_sentiment(text):
     text = text.lower()
     text = clean_text(text)
     text = remove_digits(text)
@@ -83,3 +87,46 @@ def predict_sentiment_svm(text):
     text_vector = vectorizer.transform([text])
     prediction = model.predict(text_vector)
     return prediction.tolist()[0]
+
+
+def predict_credit(code_gender, flag_own_car, flag_own_realty, cnt_children, amt_income_total, name_income_type, name_education_type, name_family_status, name_housing_type, days_birth, days_employed, flag_work_phone, flag_phone, flag_email, occupation_type, cnt_fam_members, months_balance):
+    with open("random_forest_model.pkl", "rb") as f:
+        random_forest_model = pickle.load(f)
+
+    input_data = pd.DataFrame({
+        "CODE_GENDER": [code_gender],
+        "FLAG_OWN_CAR": [flag_own_car],
+        "FLAG_OWN_REALTY": [flag_own_realty],
+        "CNT_CHILDREN": [cnt_children],
+        "AMT_INCOME_TOTAL": [amt_income_total],
+        "NAME_INCOME_TYPE": [name_income_type],
+        "NAME_EDUCATION_TYPE": [name_education_type],
+        "NAME_FAMILY_STATUS": [name_family_status],
+        "NAME_HOUSING_TYPE": [name_housing_type],
+        "DAYS_BIRTH": [days_birth],
+        "DAYS_EMPLOYED": [days_employed],
+        "FLAG_WORK_PHONE": [flag_work_phone],
+        "FLAG_PHONE": [flag_phone],
+        "FLAG_EMAIL": [flag_email],
+        "OCCUPATION_TYPE": [occupation_type],
+        "CNT_FAM_MEMBERS": [cnt_fam_members],
+        "MONTHS_BALANCE": [months_balance]
+    })
+
+    scaler = MinMaxScaler()
+    input_data[["AMT_INCOME_TOTAL", "DAYS_EMPLOYED", "DAYS_BIRTH"]] = scaler.fit_transform(input_data[["AMT_INCOME_TOTAL", "DAYS_EMPLOYED", "DAYS_BIRTH"]])
+
+    columns_to_encode = ["CODE_GENDER", "FLAG_OWN_CAR", "FLAG_OWN_REALTY"]
+    one_hot_encoder = OneHotEncoder(drop=None, sparse=False)
+    encoded_data = one_hot_encoder.fit_transform(input_data[columns_to_encode])
+    input_data_encoded = pd.DataFrame(encoded_data, columns=one_hot_encoder.get_feature_names_out(columns_to_encode))
+    input_data = pd.concat([input_data, input_data_encoded], axis=1)
+    input_data = input_data.drop(columns=columns_to_encode)
+
+    label_encoder = LabelEncoder()
+    for column in ["NAME_INCOME_TYPE", "NAME_EDUCATION_TYPE", "NAME_FAMILY_STATUS", "NAME_HOUSING_TYPE", "OCCUPATION_TYPE"]:
+        input_data[column] = label_encoder.fit_transform(input_data[column])
+
+    prediction = random_forest_model.predict(input_data)
+
+    return prediction[0]
